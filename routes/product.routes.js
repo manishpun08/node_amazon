1c83f5c94f724c367fa8031b58d9d1b6f085fe2c
manishpun08 <manishpunmagar6@gmail.com>
2024-01-05 18:26:05 +0545

    added edit and delete to product and seller

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -17,7 +17,43 @@ router.post("/product/add", (req, res) => {
 router.get("/product/list", (req, res) => {
   return res.status(200).send(productList);
 });
+//delete product
+router.delete("/product/delete/:id/", (req, res) => {
+  const productIdToBeDeleted = +req.params.id;
 
+  const newProductList = productList.filter((item, index, self) => {
+    return item.id !== productIdToBeDeleted;
+  });
+  productList = structuredClone(newProductList);
+  return res.status(200).send({ message: "Product is deleted successfully." });
+});
+
+//edit product
+router.put("/product/edit/:id", (req, res) => {
+  //extract product id to be edited from req.params
+  const productIdToEdited = +req.params.id;
+  //extract new value from req.body
+  const newValues = req.body;
+  //check if provided if exist
+  const requiredProduct = productList.find((item, index, self) => {
+    if (item.id === productIdToEdited) {
+      return item;
+    }
+  });
+  //if provided id not exist
+  if (!requiredProduct) {
+    return res.status(404).send({ message: "Product does not exist" });
+  }
+  //if provided id is matched
+  const newProductList = productList.map((item, index, self) => {
+    if (item.id === productIdToEdited) {
+      return { ...item, ...newValues };
+    } else {
+      return item;
+    }
+  });
+  productList = structuredClone(newProductList);
+  //send appropriate response
+  return res.status(200).send({ message: "Product is updated successfully." });
+});
 export default router;
-
-
--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -19,6 +19,44 @@ router.get("/seller/list", (req, res) => {
   return res.status(200).send(sellerList);
 });
 
+//delete seller
+router.delete("/seller/delete/:id/", (req, res) => {
+  const sellerIdToBeDeleted = req.params.id;
+
+  const newSellerList = sellerList.filter((item, index, self) => {
+    return item.id !== sellerIdToBeDeleted;
+  });
+  sellerList = structuredClone(newSellerList);
+  return res.status(200).send({ message: "Seller is deleted successfully." });
+});
+
 //edit seller
+router.put("/seller/edit/:id", (req, res) => {
+  //extract seller id to be edited from req.params
+  const sellerIdToBeEdited = req.params.id;
+  //extract new values from req.body
+  const newValues = req.body;
+  //check if seller with proper id exits
+  const requiredSeller = sellerList.find((item, index, self) => {
+    if (item.id === sellerIdToBeEdited) {
+      return item;
+    }
+  });
+  //if not exits, throw error
+  if (!requiredSeller) {
+    return res.status(404).send({ message: "Seller doesn't exits." });
+  }
+  //if exits, edit seller
+  const newSellerList = sellerList.map((item, index, self) => {
+    if (item.id === sellerIdToBeEdited) {
+      return { ...item, ...newValues };
+    } else {
+      return item;
+    }
+  });
+  sellerList = structuredClone(newSellerList);
+  //send appropriate response
+  return res.status(200).send({ message: "Seller is updated successfully." });
+});
 
 export default router;
